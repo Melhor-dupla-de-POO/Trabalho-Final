29e57850b74d6a8e53194efdd63553cea8aca65f
Luiz Henrique Yuji Delgado Oda <247255@dac.unicamp.br>
2022-06-07 14:34:31 -0300

    cria projeto

---
+++ Trabalho Final/desktop/src/com/poo/jogo/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.poo.jogo;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.poo.jogo.Jogo;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Trabalho Final");
+		new Lwjgl3Application(new Jogo(), config);
+	}
+}
