db2c986e38c8053d63f16b380143fd882dcd7296
Luiz Henrique Yuji Delgado Oda <l247255@dac.unicamp.br>
2022-06-22 14:55:43 -0300

    muda versão do jre

--- Trabalho Final/core/src/com/poo/jogo/Celula.java
+++ Trabalho Final/core/src/com/poo/jogo/Celula.java
@@ -6,13 +6,13 @@ import java.util.Random;
 public class Celula {
 	boolean comida;
 	
-	ArrayList<Especie> esp1;
+	/*ArrayList<Especie> esp1;
 	ArrayList<Especie> esp2;
 	ArrayList<Especie> esp3;
 	ArrayList<Especie> esp4;
-	
+	*/
 	void resetaComida() {
-		Random rand = new Random;
+		Random rand = new Random();
 		int rnumb = rand.nextInt(10);
 		if(rnumb == 0) this.comida = true;
 		else this.comida = false;
